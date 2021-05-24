c60d09406f389c2fd3e5cbfa28abc898e74a2d7c
arthsan <arthur_sbd@hotmail.com>
2021-05-24 15:09:28 -0300

    nextjs

---
+++ react/03-ignews/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from "../styles/home.module.scss";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <h1 className={styles.title}>Hello <span>World</span></h1>
+    </>
+  );
+}
