c1fa29e519243ec5b76a4cc2679fa1a684e13437
arthsan <arthur_sbd@hotmail.com>
2021-06-16 00:19:18 -0300

    react query pagination

--- react/04-dashgo/src/pages/users/create.tsx
+++ react/04-dashgo/src/pages/users/create.tsx
@@ -12,9 +12,13 @@ import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import Link from "next/link";
+import { useMutation } from "react-query";
 import { SubmitHandler, useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/dist/client/router";
 
 type CreateUserFormData = {
   name: string;
@@ -35,14 +39,39 @@ const createUserFormSchema = yup.object().shape({
     .oneOf([null, yup.ref("password")], "As senhas precisam ser iguais"),
 });
 
-const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-};
-
 export default function CreateUser() {
+  const router = useRouter();
+
+  const createUser = useMutation(
+    async (user: CreateUserFormData) => {
+      const response = await api.post("users", {
+        user: {
+          ...user,
+          created_at: new Date(),
+        },
+      });
+
+      return response.data.user;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("users");
+      },
+    }
+  );
+
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (
+    values
+  ) => {
+    await createUser.mutateAsync(values);
+
+    router.push("/users");
+  };
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema),
   });
+
   return (
     <Box>
       <Header />
